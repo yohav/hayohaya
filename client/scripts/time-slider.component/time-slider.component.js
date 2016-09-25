33cb0023f934293e5e1c463c423e3efae77fe822
moshbuog <moshe.berchansky@hotmail.com>
2016-09-25 13:20:25 +0300

    add time slide component

---
+++ client/scripts/time-slider.component/time-slider.component.js
@@ -0,0 +1,29 @@
+/**
+ * Created by lenovo on 9/25/2016.
+ */
+(function(angular) {
+    angular.module('time-slider-module', [])
+
+        .component('time-slider',{
+            template:`
+                <md-slider-container>
+                    <span>R</span>
+                    fjesfhdshklfbds
+                    gds
+                    g
+                    fg
+                    fdg
+                    fd
+                    g
+                    <md-slider flex min="0" max="255" ng-model="color.red" aria-label="red" id="red-slider">
+                    </md-slider>
+                </md-slider-container>
+            `,
+            bindings: { $router: '<' },
+            controller:'TimeSliderComponent'
+        });
+
+    function TimeSliderComponent() {
+        var vm=this;
+    }
+})(window.angular);
