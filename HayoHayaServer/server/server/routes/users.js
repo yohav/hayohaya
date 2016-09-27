f0544a5a7b351760f7d47478a8392ac1c0cc097a
origilad <User@GIL-AD>
2016-09-27 20:56:16 +0300

    remove spaces

--- HayoHayaServer/server/server/routes/users.js
+++ HayoHayaServer/server/server/routes/users.js
@@ -71,7 +71,7 @@ router.get('/rank/:id', function (req, res, next) {
     var details = req.body;
     User.findById(id, function (err, user) {
         var averageRank = calculateRank(user.rank);
-        res.status(200).send({ averageRank });
+        res.status(200).send({averageRank});
     })
 });
 
@@ -82,7 +82,7 @@ router.post('/rank/:id', function (req, res, next) {
         user.rank.push(details.rank);
         user.save();
         var averageRank = calculateRank(user.rank);
-        res.status(200).send({ averageRank });
+        res.status(200).send({averageRank});
     })
 });
 
